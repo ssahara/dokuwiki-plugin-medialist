f83bc5d342e1c188f0444b1bb9476cc5e72053ae
Satoshi Sahara <sahara.satoshi@gmail.com>
2016-06-30 21:26:57 +0900

    remove unused handle_content_display() in action
    TPL_CONTENT_DISPLAY event handler seems not applicable to sidebar page?

--- action.php
+++ action.php
@@ -18,9 +18,6 @@ class action_plugin_medialist extends DokuWiki_Action_Plugin {
         $controller->register_hook(
             'RENDERER_CONTENT_POSTPROCESS', 'AFTER', $this, 'handle_postprocess', array()
         );
-        $controller->register_hook(
-            'TPL_CONTENT_DISPLAY', 'BEFORE', $this, 'handle_content_display', array()
-        );
     }
 
 
@@ -46,33 +43,6 @@ class action_plugin_medialist extends DokuWiki_Action_Plugin {
                     return $medialist->render_xhtml($params);
                 },
                 $event->data[1]
-            );
-            return true;
-        }
-    }
-
-    /**
-     * handler of content display : Post process the XHTML output
-     *
-     * replace medialst placeholders in xhtml of the page
-     */
-    public function handle_content_display(Doku_Event $event, $param) {
-        global $ACT;
-
-        if ($ACT != 'show') return;
-
-        $pattern = '#<!-- MEDIALIST ([^\r\n]+?) -->#';
-        if (strpos($event->data, substr($pattern, 1, 15)) !== false) {
-
-            // regular expression search and replace using anonymous function callback
-            $event->data = preg_replace_callback( $pattern,
-                function ($matches) {
-                    $medialist = $this->loadHelper('medialist');
-                    $data = '{{medialist>'.$matches[1].'}}';
-                    $params = $medialist->parse($data);
-                    return $medialist->render_xhtml($params);
-                },
-                $event->data
             );
             return true;
         }
